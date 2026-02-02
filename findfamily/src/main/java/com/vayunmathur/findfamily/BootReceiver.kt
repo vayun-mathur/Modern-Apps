2a17e76c3e0bb12be37e99dfdf34e7730c8d9ebb
Vayun <69178052+vayun-mathur@users.noreply.github.com>
2026-02-01 16:27:26 -0800

    findfamily: boot receiver on updating package

--- findfamily/src/main/java/com/vayunmathur/findfamily/BootReceiver.kt
+++ findfamily/src/main/java/com/vayunmathur/findfamily/BootReceiver.kt
@@ -6,7 +6,7 @@ import android.content.Intent
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED || intent.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
             context.startForegroundService(Intent(context, LocationTrackingService::class.java))
         }
     }
