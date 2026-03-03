f7d54ba4db6d07aed18f328823c6dbb150f40a63
Vayun Mathur <69178052+vayun-mathur@users.noreply.github.com>
2026-03-03 13:42:37 -0800

    general: updated versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -150,23 +150,9 @@ subprojects {
 
         "implementation"(libs.findLibrary("androidx-compose-ui").get())
         "implementation"(libs.findLibrary("androidx-compose-ui-graphics").get())
-        "implementation"(libs.findLibrary("androidx-compose-ui-tooling-preview").get())
         "implementation"(libs.findLibrary("androidx-compose-material3").get())
 
         // Kotlin Serialization
         "implementation"(libs.findLibrary("kotlinx-serialization-json").get())
-    }
-    dependencies {
-//        "testImplementation"(libs.findLibrary("junit").get())
-//        "androidTestImplementation"(libs.findLibrary("androidx.junit").get())
-//        "androidTestImplementation"(libs.findLibrary("androidx.espresso.core").get())
-//
-//        // Handling the BOM
-//        val composeBom = libs.findLibrary("androidx.compose.bom").get()
-//        "androidTestImplementation"(platform(composeBom))
-//
-//        "androidTestImplementation"(libs.findLibrary("androidx.compose.ui.test.junit4").get())
-        "debugImplementation"(libs.findLibrary("androidx.compose.ui.tooling").get())
-        "debugImplementation"(libs.findLibrary("androidx.compose.ui.test.manifest").get())
     }
 }
